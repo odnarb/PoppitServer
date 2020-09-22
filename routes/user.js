b7cdc6bb7e1d7d4bdfb0349915e020239f03e747
Brandon Chambers <bran.cham@gmail.com>
2020-09-21 20:00:32 -0700

    cleanup - moved routes to their own area.

---
+++ routes/user.js
@@ -0,0 +1,59 @@
+//user routes
+
+let express = require('express');
+let router = express.Router();
+
+// user/login
+router.get('/login', function(req, res) {
+    return res.render('pages/login',{
+        data: {
+            pageTitle: process.env.APP_NAME + ' | Login'
+        }
+    });
+});
+
+// user/login
+router.post('/login', function(req, res) {
+    if( !req.body ){
+        return res.status(400).json({reason: "no_params_sent"});
+    } else if (!req.body.email){
+        return res.status(400).json({ reason: "no_email" });
+    } else if (!req.body.password){
+        return res.status(400).json({ reason: "no_password" });
+    }
+
+    Users.find({ email: req.body.email }, function(err,user) {
+        if(err){
+            console.log( getTime() + " - DB User.find() error: ", err);
+            return res.status(500).json({reason: "server_error"});
+        }
+
+        //user not found at all
+        if( user == undefined ){
+            return res.status(400).json({reason: "no_user"});
+        }
+
+        if( bcrypt.compareSync(req.body.password, user.password_hash) ){
+
+            //only check if the passwords match
+            //user has not been activated
+            if( user.active == 0 ){
+                return res.status(400).json({reason: "not_active"});
+            }
+
+            req.session.regenerate(function(err) {
+                console.log( getTime() + ' - : User logged IN' );
+                req.session.isLoggedIn = true;
+                req.session.user = user;
+                if( req.body.remember && req.body.remember == "on" ){
+                    req.session.cookie.maxAge = COOKIE_MAX_AGE;
+                }
+                return res.send({ result: user });
+            });
+        } else {
+            return res.status(400).json({reason: "no_user"});
+        }
+    })
+});
+
+module.exports = router;
