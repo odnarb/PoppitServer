bfbaf596a2a575b42999c44505ff074b123763dd
Brandon Chambers <bran.cham@gmail.com>
2020-09-25 07:20:53 -0700

    Forever was hanging because of opption switches.

--- scripts/forever-start.sh
+++ scripts/forever-start.sh
@@ -3,4 +3,4 @@
 # this script could be copied after the build process to a startup script location
 
 #start app script with forever and piping a port
-forever --minUptime 3000 --spinSleepTime 3000 start __PROJECT_PATH__/scripts/forever-config.json
+forever start __PROJECT_PATH__/scripts/forever-config.json
