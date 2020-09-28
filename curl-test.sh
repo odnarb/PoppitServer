5efde169d5f28a3922892067ec90834f2f20bba4
Brandon Chambers <bran.cham@gmail.com>
2020-09-28 09:37:09 -0700

    testing - updated curl tests

--- curl-test.sh
+++ curl-test.sh
@@ -11,10 +11,10 @@ curl -w "\n" -H "X-Requested-With: XMLHttpRequest" http://localhost:7777/company
 curl -w "\n" -I http://localhost:7777/company/1
 
 # PUT /company/:id route for editing a company
-curl -w "\n" -X PUT http://localhost:7777/company/1 --data '{ test: 123 }'
+curl -w "\n" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X PUT http://localhost:7777/company/1 --data '{ "name": "Acme 123","description": "This is a test company","address":"123 Nowhere","city":"Tucson","state":"AZ","zip":"85737" }'
 
 # DELETE /company/:id route for deleting a company
-curl -w "\n" -X PUT http://localhost:7777/company/1
+curl -w "\n" -X DELETE http://localhost:7777/company/1
 
 # POST /company route for creating a company
 curl -w "\n" -X POST http://localhost:7777/company --data '{ name: "Acme", description: "test Acme company" }'
