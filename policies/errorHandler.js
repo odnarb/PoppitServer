36ed29c07227471f7893b18f9a558144b25816c4
Brandon Chambers <bran.cham@gmail.com>
2020-09-28 09:03:27 -0700

    Error handler - Handle ajax errors and http separately. (render page vs sending json)

--- policies/errorHandler.js
+++ policies/errorHandler.js
@@ -1,19 +1,31 @@
 module.exports = (globals) => {
     main: return (error, req, res, next) => {
         if(error) {
-            globals.logger.error("ERROR: ", error);
+            globals.logger.debug("ERROR HANDLER: error: ", error);
+            globals.logger.debug("ERROR HANDLER: res? ", res.statusCode);
 
-            let errPage = globals.errorPages[error.status];
+            if( req.xhr ) {
+                globals.logger.debug("ERROR HANDLER :: XHR/AJAX ERROR :: ", error);
+                globals.logger.error("ERROR HANDLER:: XHR/AJAX :: ", error);
 
-            if ( errPage === undefined ) {
-                errPage = 'generalError';
+                const util = require('util');
+                return res.status(res.statusCode).json({ status_code: error.status, status: "error", err_msg: error })
+            } else {
+                globals.logger.debug("ERROR HANDLER :: HTTP :: ", error);
+                globals.logger.error("ERROR HANDLER:: HTTP :: ", error);
+
+                let errPage = globals.errorPages[error.status];
+                if ( errPage === undefined ) {
+                    errPage = 'generalError';
+                }
+
+                globals.logger.debug(`ERROR HANDLER :: HTTP ERROR :: Error ${error.status} :: ${req.method} ${req.url}`);
+
+                let errPageStr = 'errors/'+errPage+'.ejs';
+                return res.status(error.status).render(errPageStr);
             }
-
-            globals.logger.error(`Error ${error.status} :: ${req.method} ${req.url}`);
-
-            let errPageStr = 'errors/'+errPage+'.ejs';
-            return res.status(error.status).render(errPageStr);
         }
+
         next();
     }
 };
