bd5d51673703ef06adfae29c69fd2a0bc1763336
Brandon Chambers <bran.cham@gmail.com>
2020-09-28 22:35:46 -0700

    tests - updated curl tests

--- curl-test.sh
+++ curl-test.sh
@@ -8,13 +8,13 @@ curl -w "\n" -I http://localhost:7777/company
 curl -w "\n" -H "X-Requested-With: XMLHttpRequest" http://localhost:7777/company
 
 # GET /company/:id route for getting a company
-curl -w "\n" -I http://localhost:7777/company/1
+curl curl -w "\n" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" http://localhost:7777/company/1
 
 # PUT /company/:id route for editing a company
 curl -w "\n" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X PUT http://localhost:7777/company/1 --data '{ "name": "Acme 123","description": "This is a test company","address":"123 Nowhere","city":"Tucson","state":"AZ","zip":"85737" }'
 
 # DELETE /company/:id route for deleting a company
-curl -w "\n" -X DELETE http://localhost:7777/company/1
+curl -w "\n" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X DELETE http://localhost:7777/company/1
 
 # POST /company route for creating a company
-curl -w "\n" -X POST http://localhost:7777/company --data '{ name: "Acme", description: "test Acme company" }'
+curl -w "\n" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X POST http://localhost:7777/company --data "{ \"name\":\"Acme\",\"description\":\"this isn't just a test Acme company\",\"address\":\"321 Nowhere Dr.\",\"city\":\"Phoenix\",\"state\":\"AZ\",\"zip\":\"12345\"}"
