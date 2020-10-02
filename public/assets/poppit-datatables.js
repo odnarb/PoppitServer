3442307fec6d052ac3dc2bce4bc0291c590fb290
Brandon Chambers <bran.cham@gmail.com>
2020-10-02 08:20:23 -0700

    pop-37 - Basic support for modals

--- public/assets/poppit-datatables.js
+++ public/assets/poppit-datatables.js
@@ -1,6 +1,9 @@
 "use strict";
 var KTDatatablesExtensionsKeytable = function() {
 
+    // $('#kt_company_modal').modal('show');
+    var modal = $('#kt_company_modal');
+
     var initCompanyTable = function() {
         // begin first table
         var table = $('#kt_table_1').DataTable({
@@ -36,34 +39,59 @@ var KTDatatablesExtensionsKeytable = function() {
                     targets: -1,
                     title: 'Actions',
                     orderable: false,
-                    render: function(data, type, full, meta) {
+                    render: function(data, type, company, meta) {
                         return `
                         <span class="dropdown">
                             <a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md" data-toggle="dropdown" aria-expanded="true">
                               <i class="la la-ellipsis-h"></i>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right">
-                                <a class="dropdown-item" href="#"><i class="la la-edit"></i> Edit Company</a>
-                                <a class="dropdown-item" href="#"><i class="la la-remove"></i> Delete Company</a>
+                                <a class="dropdown-item edit-company" href="#" data-company-id=${company.id}><i class="la la-edit"></i> Edit Company</a>
+                                <a class="dropdown-item remove-company" href="#" data-company-id=${company.id}><i class="la la-remove"></i> Delete Company</a>
                             </div>
                         </span>
-                        <a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="View All Details">
+                        <a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md view-company" title="View All Details" data-company-id=${company.id}>
                           <i class="la la-eye"></i>
                         </a>`;
-                    },
+                    }
                 }
             ]
         });
 
+        table.on( 'init', function ( e, settings, json ) {
+            // console.log("rowData: ", rowData );
+
+            $('.view-company').on('click', function(e){
+                e.preventDefault();
+                console.log("SHOW MODAL FOR COMPANY :: event: ", e)
+
+                console.log( `view company id?: ${$(event.currentTarget).data('company-id')}` );
+
+                $('#kt_company_modal').modal('show');
+            });
+            $('.remove-company').on('click', function(e){
+                e.preventDefault();
+                console.log("[REMOVE] COMPANY :: event: ", e)
+
+                console.log( `remove company id?: ${$(event.currentTarget).data('company-id')}` );
+
+            });
+            $('.edit-company').on('click', function(e){
+                e.preventDefault();
+                console.log("SHOW [EDIT] MODAL FOR COMPANY :: event: ", e)
+
+                console.log( `edit company id?: ${$(event.currentTarget).data('company-id')}` );
+
+                $('#kt_edit_company_modal').modal('show');
+            });
+        });
     };
 
     return {
-
         //main function to initiate the module
         init: function() {
             initCompanyTable();
-        },
-
+        }
     };
 
 }();
